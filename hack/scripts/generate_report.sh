446e75ea999a5ed058b9954674050cd624d52e8f
James Harrington <james@harrington.net.au>
2020-06-23 06:50:34 -0400

    Fix reporting script

--- hack/scripts/generate_report.sh
+++ hack/scripts/generate_report.sh
@@ -2,13 +2,14 @@
 
 TOTAL_REQUESTS=0
 TIME_STAMP=$(date +%d%m%Y-%H%M%S)
+NAMESPACE=iperf-operator
 
 
-DESIRED_COMPLETED_IPERF_JOBS=$(oc get iperf iperf-test -n iperf-operator -o json | jq '.spec.clientNum')
+DESIRED_COMPLETED_IPERF_JOBS=$(oc get iperf iperf-test -n "$NAMESPACE" -o json | jq '.spec.clientNum')
 DESIRED_COMPLETED_CLIENT_JOBS=$(oc get nodes -l node-role.kubernetes.io/worker="" -o json | jq '[ .items[].metadata.name ] | length')
 
-COMPLETED_IPERF_JOBS=$(oc get jobs -o json | jq '[ .items[] |  select(.metadata.name|test("iperf-client")) | select(.status.succeeded==1) | .metadata.name ] | length')
-COMPLETED_CLIENT_JOBS=$(oc get jobs -o json | jq '[ .items[] |  select(.metadata.name|test("testclient")) | select(.status.succeeded==1) | .metadata.name ] | length')
+COMPLETED_IPERF_JOBS=$(oc get jobs -n "$NAMESPACE" -o json | jq '[ .items[] |  select(.metadata.name|test("iperf-client")) | select(.status.succeeded==1) | .metadata.name ] | length')
+COMPLETED_CLIENT_JOBS=$(oc get jobs -n "$NAMESPACE" -o json | jq '[ .items[] |  select(.metadata.name|test("testclient")) | select(.status.succeeded==1) | .metadata.name ] | length')
 
 if [ "$COMPLETED_IPERF_JOBS" != "$DESIRED_COMPLETED_IPERF_JOBS" ]; then
     echo "Failed only $COMPLETED_IPERF_JOBS completed out of $DESIRED_COMPLETED_IPERF_JOBS"
@@ -20,46 +21,23 @@ if [ "$COMPLETED_CLIENT_JOBS" != "$DESIRED_COMPLETED_CLIENT_JOBS" ]; then
     exit 1
 fi
 
-for p in $(oc get jobs -n iperf-operator | grep testclient | awk '{print $1}')
-do
-    TOTAL_REQUESTS=$(expr $TOTAL_REQUESTS + $(oc logs jobs/$p -n iperf-operator | wc -l))
-done
-
-echo "Total requests: $TOTAL_REQUESTS"
-
-i=1
-
 IFS='
 '
 
-for p in $(oc get jobs -n iperf-operator | grep testclient | awk '{print $1}')
+for p in $(oc get jobs -n "$NAMESPACE" | grep testclient | awk '{print $1}')
 do
-  for log in $(oc logs jobs/$p -n iperf-operator | jq -r '. | "\(.time_total), \(.http_code), \(.num_connects)"')
+  for log in $(oc logs jobs/"$p" -n "$NAMESPACE" | jq -r '. | "\(.time_total), \(.http_code), \(.num_connects)"')
   do
-      echo "$i, $log" >> run_${TIME_STAMP}.csv
+      echo "$i, $log" >> run_"${TIME_STAMP}".csv
     ((i++))
   done
 done
 
 
 
-for p in $(oc get jobs -n iperf-operator | grep testclient | awk '{print $1}')
+for p in $(oc get jobs -n "$NAMESPACE" | grep testclient | awk '{print $1}')
 do
-    TOTAL_REQUESTS=$(expr $TOTAL_REQUESTS + $(oc logs jobs/$p -n iperf-operator | wc -l))
+    TOTAL_REQUESTS=$(("$TOTAL_REQUESTS" + $(oc logs jobs/"$p" -n "$NAMESPACE" | wc -l)))
 done
 
 echo "Total requests: $TOTAL_REQUESTS"
-
-i=1
-
-IFS='
-'
-
-for p in $(oc get jobs -n iperf-operator | grep testclient | awk '{print $1}')
-do
-  for log in $(oc logs jobs/$p -n iperf-operator | jq -r '. | "\(.time_total), \(.http_code), \(.num_connects)"')
-  do
-      echo "$i, $log" >> run_${TIME_STAMP}.csv
-    ((i++))
-  done
-done
